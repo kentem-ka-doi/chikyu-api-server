120c8d43b26f45818d6280e829c8446a7ed89838
ka-doi <ka-doi@kentem.co.jp>
2024-12-02 16:09:53 +0900

    商魂コードから会社情報を取得するAPIを追加

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,38 @@ const fetchCompanyByKentemId = async (kentemId: string) => {
     );
     return data;
   } catch (error) {
-    console.error("Error in fetchCompanies:", error);
+    console.error("Error in fetchCompanyByKentemId:", error);
+    throw error;
+  }
+};
+
+const fetchCompanyByShokonCode = async (shokonCode: string) => {
+  const collectionName: string = "companies";
+  try {
+    // ログイン
+    await Chikyu.session.login(
+      process.env.TOKEN_NAME || "",
+      process.env.TOKEN || "",
+      process.env.SECRET_TOKEN || ""
+    );
+
+    // 組織変更
+    await Chikyu.session.changeOrgan(organizationId);
+
+    // データ取得
+    const data = await Chikyu.secure.invoke(
+      `/entity/${collectionName}/single`,
+      {
+        key: shokonCode,
+        key_search_option: {
+          input_field_name: "shokon_code",
+          input_method: "by_field_value",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.error("Error in fetchCompanyByShokonCode:", error);
     throw error;
   }
 };
@@ -38,7 +69,6 @@ const fetchCompanyByKentemId = async (kentemId: string) => {
 const app = express();
 app.use(bodyParser.json());
 
-// シンプルなエンドポイント
 app.get(
   "/api/v1/companies/:kentemId",
   async (req: Request, res: Response): Promise<void> => {
@@ -51,6 +81,27 @@ app.get(
 
     try {
       const result = await fetchCompanyByKentemId(kentemId);
+      console.log(result);
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error processing your request" });
+    }
+  }
+);
+
+app.get(
+  "/api/v1/companies/shokon-code/:shokonCode",
+  async (req: Request, res: Response): Promise<void> => {
+    const { shokonCode } = req.params;
+
+    if (!shokonCode) {
+      res.status(400).json({ error: "shokon-code parameter is required" });
+      return;
+    }
+
+    try {
+      const result = await fetchCompanyByShokonCode(shokonCode);
       console.log(result);
       res.json(result);
     } catch (error) {
