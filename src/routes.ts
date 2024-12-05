1a2e4b3c0d425048bccf4751e6a552b7f61ee770
ka-doi <ka-doi@kentem.co.jp>
2024-12-05 10:18:48 +0900

    routesの順番変更

--- src/routes.ts
+++ src/routes.ts
@@ -15,27 +15,30 @@ export const defineEndpoints = (app: Application): void => {
   const businessDiscussionsHandler = createHandler("business_discussions");
 
   // Define routes for companies
-  app.get(
-    "/api/v1/companies/:kentem_id",
-    generateSingleHandler(companiesHandler, "kentem_id")
-  );
+  app.get("/api/v1/companies", companiesHandler("list")(getListParams));
+  app.post("/api/v1/companies", companiesHandler("create")(createParams));
+
   app.get(
     "/api/v1/companies/shokon-code/:shokon_code",
     generateSingleHandler(companiesHandler, "shokon_code")
   );
-  app.get("/api/v1/companies", companiesHandler("list")(getListParams));
-  app.post("/api/v1/companies", companiesHandler("create")(createParams));
+  app.patch(
+    "/api/v1/companies/shokon-code/:shokon_code",
+    patchByFieldHandler("companies", "shokon_code")
+  );
+
   app.patch(
     "/api/v1/companies/id/:key",
     companiesHandler("save")(updateParams("key"))
   );
+
+  app.get(
+    "/api/v1/companies/:kentem_id",
+    generateSingleHandler(companiesHandler, "kentem_id")
+  );
   app.patch(
     "/api/v1/companies/:kentem_id",
     patchByFieldHandler("companies", "kentem_id")
-  );
-  app.patch(
-    "/api/v1/companies/shokon-code/:shokon_code",
-    patchByFieldHandler("companies", "shokon_code")
   );
 
   // Define routes for business discussions
