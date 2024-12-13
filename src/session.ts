e4089ee4d92739a2f1eff3170c48d37e0f8c48e0
ka-doi <ka-doi@kentem.co.jp>
2024-12-13 15:37:03 +0900

    トークン生成処理追加

--- src/session.ts
+++ src/session.ts
@@ -1,15 +1,84 @@
 import Chikyu from "chikyu-sdk";
 
 let chikyuSession: any | null = null;
-let sessionExpiry: number | null = null;
+let tokenExpiry: number | null = null;
+let createSessionTokenResponse: {
+  login_secret_token: string | null;
+  login_token: string | null;
+};
+
 const organizationId = Number(process.env.ORGANIZATION_ID) || 10794;
+/**
+ * Token name / トークン名
+ */
+const tokenName = process.env.TOKEN_NAME || "";
+/**
+ * e-mail / メールアドレス
+ */
+const email = process.env.EMAIL || "";
+/**
+ * password / パスワード
+ */
+const password = process.env.PASSWORD || "";
+/**
+ * duration / 持続期間
+ * 24時間
+ */
+const duration = Number(process.env.DURATION) || 86400;
 
 // Checks if the current session is still valid / セッションが有効かをチェック
 const isSessionValid = () => {
-  if (!chikyuSession || !sessionExpiry) {
-    return false; // Invalid if no session or expiry / セッションまたは有効期限がない場合は無効
+  return (
+    Chikyu.session != null &&
+    Chikyu.session.data != null &&
+    Chikyu.session.data.sessionId &&
+    Chikyu.session.data.identityId &&
+    Chikyu.session.data.credentials != null
+  );
+};
+
+const isTokenValid = (): boolean => {
+  if (!createSessionTokenResponse || !tokenExpiry) {
+    return false; // Invalid if no token or expiry / トークンまたは有効期限がない場合は無効
   }
-  return Date.now() < sessionExpiry; // Check if current time is before expiry / 現在の時間が有効期限前か確認
+  return Date.now() < tokenExpiry; // Check if current time is before expiry / 現在の時間が有効期限前か確認
+};
+
+const hasToken = (): boolean => {
+  return !!createSessionTokenResponse;
+};
+
+// Creates a new token / トークンを新規作成
+const createToken = async () => {
+  try {
+    createSessionTokenResponse = await Chikyu.token.create(
+      tokenName,
+      email,
+      password,
+      duration
+    );
+
+    return createSessionTokenResponse;
+  } catch (error) {
+    console.error("Error create token:", error); // Log the error / エラーをログに出力
+    throw error;
+  }
+};
+
+// Renews a token / トークンを更新
+const renewToken = async () => {
+  try {
+    createSessionTokenResponse = await Chikyu.token.renew(
+      tokenName,
+      createSessionTokenResponse.login_token || "", // Token / トークン
+      createSessionTokenResponse.login_secret_token || "" // Secret token / シークレットトークン
+    );
+
+    return createSessionTokenResponse;
+  } catch (error) {
+    console.error("Error renew token:", error); // Log the error / エラーをログに出力
+    throw error;
+  }
 };
 
 // Retrieves or creates a new session / セッションを取得または新規作成
@@ -18,19 +87,27 @@ export const getSession = async () => {
     return chikyuSession; // Reuse the existing session / 既存のセッションを再利用
   }
 
+  if (!hasToken()) {
+    createSessionTokenResponse = await createToken();
+  }
+
+  if (!isTokenValid()) {
+    createSessionTokenResponse = await renewToken();
+  }
+
   try {
     // Create a new session / 新しいセッションを作成
     chikyuSession = await Chikyu.session.login(
-      process.env.TOKEN_NAME || "", // Token name / トークン名
-      process.env.TOKEN || "", // Token / トークン
-      process.env.SECRET_TOKEN || "" // Secret token / シークレットトークン
+      tokenName,
+      createSessionTokenResponse.login_token || "", // Token / トークン
+      createSessionTokenResponse.login_secret_token || "" // Secret token / シークレットトークン
     );
 
     // Fix the organization ID / 組織IDを固定
     await Chikyu.session.changeOrgan(organizationId);
 
     // Set session expiry to 1 hour / セッションの有効期限を1時間に設定
-    sessionExpiry = Date.now() + 60 * 60 * 1000;
+    tokenExpiry = Date.now() + duration * 1000;
 
     return chikyuSession;
   } catch (error) {
--- src/types/chikyu-sdk.d.ts
+++ src/types/chikyu-sdk.d.ts
@@ -2,12 +2,39 @@ declare module "chikyu-sdk" {
   interface Session {
     login(tokenName: string, token: string, secretToken: string): Promise<void>;
     changeOrgan(organizationId: number): Promise<void>;
+    data: {
+      identityId: string | null;
+      sessionId: string | null;
+      credentials: any | null;
+    };
   }
 
   interface Secure {
     invoke(endpoint: string, params: object): Promise<any>;
   }
 
+  interface Token {
+    create(
+      tokenName: string,
+      email: string,
+      password: string,
+      duration: number
+    ): Promise<{
+      login_secret_token: string | null;
+      login_token: string | null;
+    }>;
+
+    renew(
+      tokenName: string,
+      loginToken: string,
+      loginSecretToken: string
+    ): Promise<{
+      login_secret_token: string | null;
+      login_token: string | null;
+    }>;
+  }
+
   export const session: Session;
   export const secure: Secure;
+  export const token: Token;
 }
